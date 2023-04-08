fb883c3769482bf160e15a10022a30b1d2f1d0a2
Steveplays28 <djspaargaren@outlook.com>
2023-04-08 15:12:14 +0200

    fix: :bug: Add checks to see if the recipes and technologies exist

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,25 +2,29 @@
 -- Disable recipes
 
 -- Base "Long-handed inserter"
-data.raw["recipe"]["long-handed-inserter"].enabled = false
-data.raw["recipe"]["long-handed-inserter"].hidden = true
-data.raw["recipe"]["long-handed-inserter"].hide_from_player_crafting = true
+if data.raw["recipe"]["long-handed-inserter"] ~= nil then
+	data.raw["recipe"]["long-handed-inserter"].enabled = false
+	data.raw["recipe"]["long-handed-inserter"].hidden = true
+	data.raw["recipe"]["long-handed-inserter"].hide_from_player_crafting = true
+end
 
 -- Renai Transportation "Thrower Long-handed inserter"
-data.raw["recipe"]["RTThrower-long-handed-inserter"].enabled = false
-data.raw["recipe"]["RTThrower-long-handed-inserter"].hidden = true
-data.raw["recipe"]["RTThrower-long-handed-inserter"].hide_from_player_crafting = true
+if data.raw["recipe"]["RTThrower-long-handed-inserter"] ~= nil then
+	data.raw["recipe"]["RTThrower-long-handed-inserter"].enabled = false
+	data.raw["recipe"]["RTThrower-long-handed-inserter"].hidden = true
+	data.raw["recipe"]["RTThrower-long-handed-inserter"].hide_from_player_crafting = true
+end
 
 
 ------------------------------------------
 -- Remove recipe unlocks from technologies
 
 -- Base "Automation"
-if data.raw["technology"]["automation"] then
+if data.raw["technology"]["automation"] ~= nil then
 	data.raw["technology"]["automation"].effects[2] = nil
 end
 
 -- Renai Transportation "Thrower Varients"
-if data.raw["technology"]["RTThrowerTime"] then
+if data.raw["technology"]["RTThrowerTime"] ~= nil then
 	data.raw["technology"]["RTThrowerTime"].effects[3] = nil
 end
